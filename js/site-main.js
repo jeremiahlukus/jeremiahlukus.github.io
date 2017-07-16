8e62d60bd122aaa0ed49d2d18ec5d334e2508026
jeremiahlukus <jeremiahlukus1@gmail.com>
2017-07-16 13:55:57 -0400

    Cleaned up

--- js/site-main.js
+++ js/site-main.js
@@ -1,6 +1,12 @@
-(function () {
-
-  var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+(function() {
+  var width,
+    height,
+    largeHeader,
+    canvas,
+    ctx,
+    points,
+    target,
+    animateHeader = true;
 
   initHeader();
   initAnimation();
@@ -14,13 +20,13 @@
       y: height / 2
     };
 
-    largeHeader = document.getElementById('large-header');
-    largeHeader.style.height = height + 'px';
+    largeHeader = document.getElementById("large-header");
+    largeHeader.style.height = height + "px";
 
-    canvas = document.getElementById('demo-canvas');
+    canvas = document.getElementById("demo-canvas");
     canvas.width = width;
     canvas.height = height;
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext("2d");
 
     points = [];
     for (var x = 0; x < width; x = x + width / 20) {
@@ -41,7 +47,7 @@
       var closest = [];
       var p1 = points[i];
       for (var j = 0; j < points.length; j++) {
-        var p2 = points[j]
+        var p2 = points[j];
         if (!(p1 == p2)) {
           var placed = false;
           for (var k = 0; k < 5; k++) {
@@ -67,27 +73,37 @@
     }
 
     for (var i in points) {
-      var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.3)');
+      var c = new Circle(
+        points[i],
+        2 + Math.random() * 2,
+        "rgba(255,255,255,0.3)"
+      );
       points[i].circle = c;
     }
   }
 
   function addListeners() {
-    if (!('ontouchstart' in window)) {
-      window.addEventListener('mousemove', mouseMove);
+    if (!("ontouchstart" in window)) {
+      window.addEventListener("mousemove", mouseMove);
     }
-    window.addEventListener('scroll', scrollCheck);
-    window.addEventListener('resize', resize);
+    window.addEventListener("scroll", scrollCheck);
+    window.addEventListener("resize", resize);
   }
 
   function mouseMove(e) {
-    var posx = posy = 0;
+    var posx = (posy = 0);
     if (e.pageX || e.pageY) {
       posx = e.pageX;
       posy = e.pageY;
     } else if (e.clientX || e.clientY) {
-      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+      posx =
+        e.clientX +
+        document.body.scrollLeft +
+        document.documentElement.scrollLeft;
+      posy =
+        e.clientY +
+        document.body.scrollTop +
+        document.documentElement.scrollTop;
     }
     target.x = posx;
     target.y = posy;
@@ -101,7 +117,7 @@
   function resize() {
     width = window.innerWidth;
     height = window.innerHeight;
-    largeHeader.style.height = height + 'px';
+    largeHeader.style.height = height + "px";
     canvas.width = width;
     canvas.height = height;
   }
@@ -143,7 +159,7 @@
       x: p.originX - 50 + Math.random() * 100,
       y: p.originY - 50 + Math.random() * 100,
       ease: Circ.easeInOut,
-      onComplete: function () {
+      onComplete: function() {
         shiftPoint(p);
       }
     });
@@ -155,7 +171,7 @@
       ctx.beginPath();
       ctx.moveTo(p.x, p.y);
       ctx.lineTo(p.closest[i].x, p.closest[i].y);
-      ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
+      ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
       ctx.stroke();
     }
   }
@@ -163,17 +179,17 @@
   function Circle(pos, rad, color) {
     var _this = this;
 
-    (function () {
+    (function() {
       _this.pos = pos || null;
       _this.radius = rad || null;
       _this.color = color || null;
     })();
 
-    this.draw = function () {
+    this.draw = function() {
       if (!_this.active) return;
       ctx.beginPath();
       ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
+      ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
       ctx.fill();
     };
   }
@@ -181,5 +197,4 @@
   function getDistance(p1, p2) {
     return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
   }
-
 })();
